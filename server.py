50d2e69bdfd598b465c36a1b009c193b1ba39a02
andimeon <andimeon@gmail.com>
2021-03-29 22:31:52 +0900

    Fix minor bugs

--- server.py
+++ server.py
@@ -3,17 +3,21 @@ from pathlib import Path, PurePath
 import asyncio
 import aiofiles
 import logging
-from subprocess import PIPE
 import argparse
 
 
 async def archivate(request):
-    archive_hash = request.match_info.get('archive_hash', "Anonymous")
+    photo_catalog_path = request.app['photo_catalog_path']
+    delay_time = request.app['delay_time']
+    archive_hash = request.match_info.get('archive_hash', 'Anonymous')
     if not Path(f'{photo_catalog_path}/{archive_hash}').exists():
-        return web.HTTPNotFound(text='Sorry, this archive is not exist or was removed')
-        
+        return web.HTTPNotFound(
+            text='Sorry, this archive is not exist or was removed')
+    
     process = await asyncio.subprocess.create_subprocess_exec(
-        'zip', '-r9', '-', f'{archive_hash}', stdout=asyncio.subprocess.PIPE, cwd=photo_catalog_path)
+        'zip', '-r9', '-', f'{archive_hash}',
+        stdout=asyncio.subprocess.PIPE,
+        cwd=photo_catalog_path)
 
     response = web.StreamResponse(
         status=200,
@@ -37,12 +41,13 @@ async def archivate(request):
             await asyncio.sleep(delay_time)
     except asyncio.CancelledError:
         logging.error('Download was interrupted')
+        raise
     finally:
         process.kill()
         await process.communicate()
-    
+   
     return response
-        
+       
 
 async def handle_index_page(request):
     async with aiofiles.open('index.html', mode='r') as index_file:
@@ -50,34 +55,49 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
-def createParser():
+def create_parser():
     parser = argparse.ArgumentParser(description='Download zip archive')
-    parser.add_argument('-l', '--log', action='store_const', const=True, help='Logging enabled', default=False)
-    parser.add_argument('-d', '--delay', help='Server response delay (max - 5sec)', default=0, type=int)
-    parser.add_argument('-p', '--path', help='Path to photo catalog', default='test_photos')
+    parser.add_argument(
+        '-l',
+        '--log',
+        action='store_const',
+        const=True,
+        help='Logging enabled',
+        default=False,
+    )
+    parser.add_argument(
+        '-d',
+        '--delay',
+        help='Server response delay (max - 5sec)',
+        default=0,
+        type=int
+    )
+    parser.add_argument(
+        '-p',
+        '--path',
+        help='Path to photo catalog', 
+        default='test_photos')
 
     return parser
 
 
 def main():
-    global delay_time
-    global photo_catalog_path
-    parser = createParser()
+    parser = create_parser()
     args = parser.parse_args()
+    app = web.Application()
 
     if args.log:
-        logging.basicConfig(level = logging.INFO)
+        logging.basicConfig(level=logging.INFO)
     else:
         logging.disable(logging.CRITICAL)
 
     if args.delay > 5:
-        delay_time = 5
+        app['delay_time'] = 5
     else:
-        delay_time = arg.delay
+        app['delay_time'] = args.delay
 
-    photo_catalog_path = PurePath(args.path)
-
-    app = web.Application()
+    app['photo_catalog_path'] = PurePath(args.path)
+    
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archivate),
